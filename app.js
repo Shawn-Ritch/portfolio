ab836310be7486da95b1a9c7488a676d83600137
Shawn Ritch <sr@webdeveloprz.com>
2020-08-13 19:45:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 function parallax() {
   var paraBgImg = document.getElementById('paraBgImg');
   var parah1 = document.getElementById('parah1');
-  paraBgImg.style.top = -(window.pageYOffset / 3)+'px';
-  parah1.style.top = -(window.pageYOffset / 6)+'px';
+  paraBgImg.style.top = -(window.pageYOffset / 2.5)+'px';
+  parah1.style.top = -(window.pageYOffset / 4)+'px';
 } window.addEventListener("scroll", parallax, false);
 
 var navLinks = document.querySelectorAll("li a");
